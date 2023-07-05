590cd9f3b3b064cd62077446ef60badab55a54ae
balbader <basilalbapro@gmail.com>
2023-07-05 21:59:00 +0200

    gnle info

---
+++ info/sigaction_default.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <signal.h>
+
+int main(void)
+{
+   struct sigaction sa;
+  
+   sigemptyset(&sa.sa_mask);
+   sa.sa_flags = SA_RESTART | SA_SIGINFO;
+   sa.sa_handler = SIG_DFL;
+   sigaction(SIGINT, &sa, NULL);
+   while (1)
+     sleep(1);
+   return (0);
+}
+
