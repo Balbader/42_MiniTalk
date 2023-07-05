4cc783a27c1b6033a5a853417ded2b817a393c8f
balbader <basilalbapro@gmail.com>
2023-07-05 15:19:21 +0200

    .h

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -16,5 +16,23 @@
 # include <signal.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdarg.h>
+# include <stddef.h>
 # include <fcntl.h>
+
+// client
+void	ft_check_signal(int kill_ref);
+void	ft_post_str(char *str, int str_len, int pid);
+
+// server
+
+// ft_printf
+int				ft_printf(const char *str, ...);
+void			ft_print_nb(int nb, int *tot_len);
+void			ft_print_hexa(unsigned int nb, char c, int *tot_len);
+void			ft_print_unsigned(unsigned int u, int *tot_len);
+void			ft_print_ptr(size_t ptr, int *tot_len);
+void			ft_print_char(char c, int *tot_len);
+void			ft_print_str(char *str, int *tot_len);
+
 #endif
