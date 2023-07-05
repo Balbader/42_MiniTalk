a950436d7132c474b575f04557f3e1e319c75581
balbader <basilalbapro@gmail.com>
2023-07-05 17:48:05 +0200

    server.c

--- srcs/server.c
+++ srcs/server.c
@@ -11,3 +11,36 @@
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	ft_print_signal(int signum)
+{
+	static char	sig;
+	static int	count;
+
+	++count;
+	if (signum == SIGUSR1)
+		sig = sig * 2 + 0;
+	else
+		sig = sig * 2 + 1;
+	if (count == 8)
+	{
+		ft_putchar(sig);
+		sig = '\0';
+		count = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_printf("SERVER PID : %d\n", getpid());
+	// signal(SIGUSR1, ft_print_signal);
+	// signal(SIGUSR2, ft_print_signal);
+	while (1)
+	{
+		signal(SIGUSR1, ft_print_signal);
+		signal(SIGUSR2, ft_print_signal);
+		pause();
+
+	}
+	return (0);
+}
