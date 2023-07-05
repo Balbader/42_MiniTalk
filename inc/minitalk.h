352f628c015e4192a41bcc0a56493ea10614190a
balbader <basilalbapro@gmail.com>
2023-07-05 13:00:02 +0200

    .h

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -11,4 +11,9 @@
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
+# define MINITALK_H
+# include <sys/types.h>
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
 #endif
